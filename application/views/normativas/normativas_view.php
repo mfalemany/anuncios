2939c070049aa2a6f8d9c205eba3d94e55143dfa
Federico Alemany <mfalemany@gmail.com>
2021-03-23 17:38:35 -0300

    Normativas Se cargan las primeras normatias

--- application/views/normativas/normativas_view.php
+++ application/views/normativas/normativas_view.php
@@ -6,7 +6,7 @@
 		</div>
 		<div class="cuerpo_seccion">
 			<ul>
-				<li><a href="<?php echo base_url(); ?>assets/pdfs/perm/resol_316_19.pdf">Resol. 316/19 C.S.</a> - Régimen de regularidad de estudiantes</li>
+				<li><a href="<?php echo base_url(); ?>assets/pdfs/perm/resol_316_19.pdf" target="_blank">Resol. 316/19 C.S.</a> - Régimen de regularidad de estudiantes</li>
 			</ul>
 		</div>
 	</section>
@@ -16,8 +16,8 @@
 		</div>
 		<div class="cuerpo_seccion">
 			<ul>
-				<li><a href="<?php echo base_url(); ?>assets/pdfs/perm/resol_0051_2021.pdf">Resol. 0051/21 C.S.</a> - Extensión de regularidades que vencen al 31/03/2021</li>
-				<li><a href="<?php echo base_url(); ?>assets/pdfs/perm/resol_0036_2021.pdf">Resol. 0036/21 C.S.</a> - Cursado condicional de materias del primer trimestre/cuatrimestre</li>
+				<li><a href="<?php echo base_url(); ?>assets/pdfs/perm/resol_0051_2021.pdf" target="_blank">Resol. 0051/21 C.S.</a> - Extensión de regularidades que vencen al 31/03/2021</li>
+				<li><a href="<?php echo base_url(); ?>assets/pdfs/perm/resol_0036_2021.pdf" target="_blank">Resol. 0036/21 C.S.</a> - Cursado condicional de materias del primer trimestre/cuatrimestre</li>
 			</ul>
 		</div>
 	</section>
